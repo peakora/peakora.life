12aaf2c021c2da6129f69c423c3bbaa451cbd6db
peakora <peakora.network@gmail.com>
2026-01-26 09:39:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@
 // ------------------------------------------------------
 
 document.addEventListener("DOMContentLoaded", () => {
+  // MOBILE MENU TOGGLE
+const menuToggle = document.getElementById("menuToggle");
+const nav = document.querySelector(".nav");
+
+menuToggle?.addEventListener("click", () => {
+  nav.classList.toggle("nav-open");
+});
+
   // --------------------
   // DOM ELEMENTS
   // --------------------
